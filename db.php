9ee6d1aa3929b5063a7159ec919e5426afc8f914
FarisPutraFrania <farispeep@gmail.com>
2023-06-12 10:43:28 +0700

    todo list

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$host_name = 'localhost';
+$db_user = 'zeinfari_faris';
+$db_pass = '1sampai8!';
+$db_name = 'zeinfari_faris';
+
+$con = mysqli_connect($host_name, $db_user, $db_pass, $db_name);
+
+?>
